e8bca4c15262313b02db79e59c4ae8ce42f05d45
Reza Ageng <rezaageng5@gmail.com>
2022-12-19 14:45:04 +0700

    feat(index): :sparkles: add login link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <p>im here to testing</p>
+      <div className="text-center">
+        <p>im here to testing</p>
+        <Link className="hover:bg-indigo-500 hover:text-white" href="/login">
+          Login
+        </Link>
+      </div>
     </div>
   )
 }
